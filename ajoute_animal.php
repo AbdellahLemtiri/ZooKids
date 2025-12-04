73b322e967e7c173c4cbfec646a07644df0fd3ee
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-12-04 17:18:30 +0100

    ajoute : la page ajoute animal

--- ajoute_animal.php
+++ ajoute_animal.php
@@ -1,34 +1,50 @@
-<
+<?php
+include "connect.php";
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Ajouter un Animal</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
+
 <body>
 
-<div class="container-form"> <h2>Ajouter un Animal</h2>
+    <div class="container-form">
+        <h2>Ajouter un Animal</h2>
 
-    <form method="post" action="#">
-        <label>Nom de l'animal :</label>
-        <input type="text" name="nom" required>
+        <form method="post" action="#">
+            <label>Nom de l'animal :</label>
+            <input type="text" name="nom" required>
 
-        <label>Espèce :</label>
-        <input type="text" name="espece" required>
+            <label>Espèce :</label>
+            <input type="text" name="espece" required>
 
-        <label>Âge :</label>
-        <input type="number" name="age" required>
+            <label>Âge :</label>
+            <input type="number" name="age" required>
 
-        <label>Habitat :</label>
-        <select name="habitat" required>
-           <?php 
+            <label>Habitat :</label>
+            <select name="habitat" required>
+                <?php
+                $sql = "SELECT NomHab,IdHab FROM habitat";
+                $RSULT = $connect->query($sql);
 
-            ?>
-        </select>
-        <button type="submit">Ajouter</button>
-    </form>
-</div>
+                if ($RSULT->num_rows > 0) {
+                    while ($row = $RSULT->fetch_assoc()) {
+
+                        echo "<option value=" . $row["IdHab"] . " >" . $row["NomHab"] . "</option>";
+                    }
+                } else {
+                    echo "<option value =0 >" . "Aucun habitat trouvé" . "</option>";
+                }
+                ?>
+            </select>
+            <button type="submit">Ajouter</button>
+        </form>
+    </div>
 
 </body>
+
 </html>
