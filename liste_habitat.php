5727e7cf447a9ba30153c01f004af5e6d821af94
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-12-04 14:34:25 +0100

    ajoute la fonctonalete en php pour affichier la  liste de  habitat

--- liste_habitat.php
+++ liste_habitat.php
@@ -9,7 +9,20 @@ if($connect -> connect_error){
     echo "error a la connection avec la base de donnee :".$connect -> connect_error;
     exit;
 }
+$sql = "SELECT * FROM  habitat ";
+$RSULT = $connect -> query($sql);
 
+if ($RSULT->num_rows > 0) {
+    while($row = $RSULT->fetch_assoc()) {
+        echo "<div>";
+        echo "<div>".$row['IdHab']."</div>";
+        echo "<div>".$row['NomHab']."</div>";
+        echo "<div>".$row['Description_Hab']."</div>";
+        echo "</div>";
+    }
+} else {
+    echo "<div><div colspan='3'>Aucun habitat trouvé</div></div>";
+}
 ?>
 
 
